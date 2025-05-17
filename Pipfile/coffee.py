a6bd01b36b00c1ad76ab568743a5b0c190864a35
Kelly Koome <koomekelly7@gmail.com>
2025-05-17 16:23:34 +0300

    feat: implement Coffee class with name validation

---
+++ Pipfile/coffee.py
@@ -0,0 +1,18 @@
+class Coffee:
+    def __init__(self, name):
+        self.name = name
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if len(value) < 3:
+            raise ValueError("Name must be at least 3 characters")
+        if hasattr(self, '_name'):
+            raise AttributeError("Coffee name cannot be changed after initialization")
+        self._name = value
+    
